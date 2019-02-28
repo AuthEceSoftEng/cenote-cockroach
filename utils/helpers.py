c725cc6432cefc10be0bdd25859d1f82140727db
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-02-28 12:41:39 +0200

    Small bug fix

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,11 +2,7 @@ from datetime import datetime
 
 
 def get_time_in_iso():
-    return datetime.now().isoformat()
-
-
-def time_to_datetime_in_ms(timestamp):
-    return datetime.fromtimestamp(timestamp / 1e3)
+    return datetime.utcnow().isoformat()
 
 
 def time_in_ms_to_iso(timestamp):
