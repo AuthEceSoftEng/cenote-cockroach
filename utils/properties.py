c4aa9ef9bcde51a457ae9e888e1c27326b4ca25d
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-03-06 15:04:32 +0200

    Add 'DATABASE_URL' env variable

--- utils/properties.py
+++ utils/properties.py
@@ -1,1 +1,3 @@
-DATABASE_URL = 'postgres://cockroach@83.212.104.172:26257/cenote?sslmode=disable'
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://cockroach@83.212.104.172:26257/cenote?sslmode=disable')
